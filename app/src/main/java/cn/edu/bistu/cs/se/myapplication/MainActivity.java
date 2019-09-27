874bb9f2d7d20002f32c9e301d7853409cef9ff3
lyt <1003655329@qq.com>
2019-09-27 22:42:53 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package cn.edu.bistu.cs.se.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView=findViewById(R.id.resultText);
+        Button button_0=findViewById(R.id.button_0);
+        Button button_1=findViewById(R.id.button_1);
+        Button button_2=findViewById(R.id.button_2);
+        Button button_3=findViewById(R.id.button_3);
+        Button button_4=findViewById(R.id.button_4);
+        Button button_5=findViewById(R.id.button_5);
+        Button button_6=findViewById(R.id.button_6);
+        Button button_7=findViewById(R.id);
+        Button button_8=findViewById(R.id);
+        Button button_9=findViewById(R.id);
+        Button button_add=findViewById(R.id);
+        Button button_=findViewById(R.id);
+        Button button_=findViewById(R.id);
+        Button button_=findViewById(R.id);
+        Button button_=findViewById(R.id);
+
+
+
+
+    }
+}
